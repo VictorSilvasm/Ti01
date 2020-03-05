2b617300da299733d3bb52a7a0ee46207eb0b085
JeronimoHS <jeronimo.her.s@gmail.com>
2020-03-04 19:48:39 -0600

    Modificación con API JSON

--- screen/Login.js
+++ screen/Login.js
@@ -1,21 +1,55 @@
 import React,{Component} from 'react';
-import { StyleSheet} from 'react-native';
+import { StyleSheet,ActivityIndicator,Modal,View} from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Body,Button,Item,Input,Icon } from 'native-base';
+
 
 class Login extends Component {
   constructor(props){
     super(props);
     this.state={
       nombr:'',
-      passwords:''
+      passwords:'',
+      isloading: true,
     }
   }
+
+  ConnectActivityIndicator = () =>{
+    const navegar=this.props.navigation;
+    if(this.state.isLoading == true)
+    {
+      this.setState({isLoading: false})
+    }
+    else
+    {
+      this.setState({isLoading: true})
+      setTimeout(() => {
+        navegar.navigate('Pantalla'/*,{nombres:this.state.nombr,pass:this.state.passwords}*/);
+        this.setState({isLoading: false});
+      },2000);
+    }      
+  }
+
 
 render(){
   const navegar=this.props.navigation;
   return (
     <Container>
         <Header />
+        <View style={misEstilos.MainContainer}>
+        {
+        this.state.isLoading ?
+        <Modal
+        transparent = {true}
+        animationType ={'none'}
+        visible = {this.state.isLoading}>
+          <View style = {misEstilos.modalBackground}>
+            <View style = {misEstilos.activityIndicatorWrapper}>
+              <ActivityIndicator style={{padding: 70}}/>
+            </View>
+          </View>
+        </Modal> : null
+        }
+        </View>
         <Content padder contentContainerStyle ={misEstilos.content}>
           <Card>
             <CardItem header bordered>
@@ -34,7 +68,7 @@ render(){
               </Body>
             </CardItem>
             <CardItem footer bordered>
-            <Button primary style={misEstilos.boton} onPress={() => {navegar.navigate('Pantalla',{nombres:this.state.nombr,pass:this.state.passwords});}}><Text> Entrar </Text></Button>
+            <Button primary style={misEstilos.boton} onPress={this.ConnectActivityIndicator}><Text> Entrar </Text></Button>
             </CardItem>
           </Card>
           <Text style={misEstilos.textCenter}>No tienes cuenta:</Text>
@@ -61,6 +95,25 @@ const misEstilos = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  MainContainer : {
+    justifyContent: 'center',
+  },
+  modalBackground : {
+    flex : 1,
+    alignItems: 'center',
+    flexDirection : 'column',
+    justifyContent: 'space-around',
+    backgroundColor: '#00000040'
+  },
+  activityIndicatorWrapper : {
+    backgroundColor : '#FFFFFF',
+    height : 100,
+    width : 100,
+    borderBottomEndRadius : 10,
+    display : 'flex',
+    alignItems : 'center',
+    justifyContent : 'space-around'
+  }
 });
 
 export default Login;
--- screen/Pantalla.js
+++ screen/Pantalla.js
@@ -1,4 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {View, Text, ActivityIndicator} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import {Card} from 'native-base'
+
+
+class Principal extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading : true,
+    }
+  }// end  constructor
+  //lo que esta aquí se ejecuta 
+  async componentDidMount(){
+    try{
+      const response = 
+      await fetch('https://swapi.co/api/films/?format=json')  //https://reactnative.dev/movies.json
+      const responseJson = await response.json();
+
+      this.setState({
+        isLoading : false,
+        dataSourse : responseJson.results,
+      }, function(){
+
+      });
+    }
+    catch (error){
+      console.log(error);
+    }
+  }//end componentDidMount
+
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex:1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      );
+    }//en if
+
+    return(
+      <View>
+        <FlatList
+          data = {this.state.dataSourse}
+          renderItem = {({item}) =>
+            <Card>
+              <Text>Titulo: {item.title}</Text>
+              <Text>Episodio: {item.episode_id}</Text>
+              <Text>Director: {item.director}</Text>
+            </Card>
+          }
+         keyExtractor =  {({episode_id}, index) => episode_id}
+        />
+      </View>
+    );
+  }
+}// end class
+
+export default Principal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { Component } from 'react';
 import { StyleSheet} from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Body,Button,Item, Icon,Input } from 'native-base';
 
@@ -28,6 +105,9 @@ const misEstilos = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  boton:{
+    marginLeft: '70%',
+  },
 });
 
-export default Pantalla;
+export default Pantalla;*/
